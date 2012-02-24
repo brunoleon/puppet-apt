b1ab955e53460a3da8504dfa9bbdf4084baa6db1
Bruno Léon <bruno.leon@unyonsys.com>
2012-02-24 21:55:20 +0100

    Update uyonsys puppet repository

--- manifests/repository/unyonsys.pp
+++ manifests/repository/unyonsys.pp
@@ -1,25 +1,8 @@
-class apt::repository::unyonsys (
-  $stage = pre
-  ) {
-  apt::repo { 'unyonsys':
-    url  => "http://repository.unyonsys.com", section => 'main'
-  }
-}
-
 class apt::repository::unyonsys::puppet (
   $stage = pre
   ) {
   apt::repo { 'unyonsys_puppet':
-    url  => "http://repository.unyonsys.com", section => 'main', forced_lsbdistcodename => 'puppet'
-  }
-}
-
-
-class apt::repository::unyonsys::crossrelease (
-  $stage = pre
-  ) {
-  apt::repo { 'unyonsys_crossrelease':
-    url  => "http://repository.unyonsys.com", section => 'main', forced_lsbdistcodename => 'crossrelease'
+    url  => "http://repository.unyonsys.com/puppet", section => 'main'
   }
 }
 

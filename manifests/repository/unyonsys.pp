1e702dd584d9f115c033f7fff45d4ba0ac6dd8ff
Bruno Léon <bruno.leon@unyonsys.com>
2012-03-13 16:10:07 +0100

    Add apt::repository::unyonsys::libguestfs

--- manifests/repository/unyonsys.pp
+++ manifests/repository/unyonsys.pp
@@ -1,8 +1,15 @@
 class apt::repository::unyonsys::puppet (
   $stage = pre
-  ) {
+) {
   apt::repo { 'unyonsys_puppet':
     url  => "http://repository.unyonsys.com/puppet", section => 'main'
   }
 }
 
+class apt::repository::unyonsys::libguestfs (
+  $stage = pre
+) {
+  apt::repo { 'unyonsys_libguestfs':
+    url  => "http://repository.unyonsys.com/libguestfs", section => 'main'
+  }
+}

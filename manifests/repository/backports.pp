13d1d3ee78c2c7646423182499e9952f3d9fb210
Bruno Léon <bruno.leon@unyonsys.com>
2013-03-20 15:05:28 +0100

    Remove erroneous file

--- manifests/repository/backports.pp
+++
@@ -1,5 +0,0 @@
-# Dell OMreport, cf http://linux.dell.com/repo/community/deb/latest/
-class apt::dellom inherits apt {
-    apt::repo { "dellom": url => "http://linux.dell.com/repo/community/deb/OMSA_6.3/", append_lsbdistcodename=> false, section => '/' }
-}
-

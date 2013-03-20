12ffea5537a394e6c97173a6bf030742a6d7087f
Bruno Léon <bruno.leon@unyonsys.com>
2013-03-20 14:53:57 +0100

    Remove wrong class header

--- manifests/repository/backports.pp
+++ manifests/repository/backports.pp
@@ -1,5 +1,3 @@
-# = Class: apt::backports
-#
 # Dell OMreport, cf http://linux.dell.com/repo/community/deb/latest/
 class apt::dellom inherits apt {
     apt::repo { "dellom": url => "http://linux.dell.com/repo/community/deb/OMSA_6.3/", append_lsbdistcodename=> false, section => '/' }

1f8f47c53e60de6005f18671eb46406aeca3b0b2
Bruno Léon <bruno.leon@unyonsys.com>
2015-01-15 05:52:21 -0500

    fix (repository): Inverse needs specific lucid handling

--- manifests/repository/inverse.pp
+++ manifests/repository/inverse.pp
@@ -1,5 +1,13 @@
 class apt::repository::inverse (
   $stage = pre
   ) {
-  apt::repo { 'inverse': url => 'http://inverse.ca/ubuntu', section => "${lsbdistcodename}", keyid => '810273C4' }
+  $section = $::lsbdistcodename ? {
+    'lucid'                  => 'main',
+    default                  => $::lsbdistcodename,
+  }
+  apt::repo { 'inverse':
+    url     => 'http://inverse.ca/ubuntu',
+    section => $section,
+    keyid   => '810273C4',
+  }
 }

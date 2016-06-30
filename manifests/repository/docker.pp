9e27acb2243b22b4ca256d3fde645007d742a3ba
Bruno Léon <bruno.leon@unyonsys.com>
2016-06-30 17:24:05 +0200

    feat (docker): add xenial support

--- manifests/repository/docker.pp
+++ manifests/repository/docker.pp
@@ -1,9 +1,9 @@
 class apt::repository::docker (
   $stage = pre
-  ) {
-  $supported = ['precise', 'trusty', 'wily']
+) {
+  $supported = ['precise', 'trusty', 'wily', 'xenial']
   if ! $::lsbdistcodename in $supported {
-    fail("Supported Ubuntu releases as of 2016-03-30 are ${supported}")
+    fail("Supported Ubuntu releases as of 2016-06-30 are ${supported}")
   }
   apt::repo { 'docker':
     url                    => 'https://apt.dockerproject.org/repo',
